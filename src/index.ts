2ce999ea26655cb870b94614fe0b61ef68b79742
Charles Loder <charlesloder@MacBook-Pro.local>
2020-11-27 06:37:00 -0500

    updated option handling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import { Text } from "./text";
+import { Text as Havarot } from "./text";
+
+interface TextOpts {
+  qametsQatan?: boolean;
+  longVowels?: boolean;
+  sqnmlvy?: boolean;
+  wawShureq?: boolean;
+}
+
+class Text extends Havarot {
+  constructor(
+    text: string,
+    options: TextOpts = {
+      qametsQatan: true,
+      longVowels: true,
+      sqnmlvy: false,
+      wawShureq: false,
+    }
+  ) {
+    super(text, options);
+  }
+}
 
 export { Text };
