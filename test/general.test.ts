79822f424bca9ff24b745efa443b6bb87f3db1c4
charlesLoder <charles.w.loder@gmail.com>
2021-09-04 15:28:42 -0400

    Add test for qamets qatan

--- test/general.test.ts
+++ test/general.test.ts
@@ -1,18 +1,19 @@
 import { Text } from "../src/index";
 
 describe.each`
-  description                            | hebrew                       | transliteration
-  ${"no special features"}               | ${"לֶ֬חֶם"}                  | ${"lechem"}
-  ${"gemination"}                        | ${"רַנֵּן"}                  | ${"ranen"}
-  ${"furtive patach, chet"}              | ${"מָשִׁיחַ"}                | ${"maschiach"}
-  ${"furtive patach, ayin"}              | ${"שָׁמֵ֑עַ"}                | ${"schamea"}
-  ${"furtive patach, he"}                | ${"גָבֹ֗הַּ"}                | ${"gawoa"}
-  ${"final chet w/ patach, NOT furtive"} | ${"מִתַּ֣חַת"}               | ${"mitachat"}
-  ${"final qamets-he"}                   | ${"מַלְכָּה"}                | ${"malka"}
-  ${"divine name"}                       | ${"יְהוָ֣ה"}                 | ${"Adonaj"}
-  ${"3ms suffix"}                        | ${"תֹּורֹתָֽיו"}             | ${"torotaw"}
-  ${"multiple words and passeq"}         | ${"כְּשֶׁ֣בֶת ׀ הַמֶּ֣לֶךְ"} | ${"keschewet hamelech"}
-  ${"mixed with latin chars"}            | ${"אֲבֹותֵינוּ (לְעֹולָם)"}  | ${"awotenu (leolam)"}
+  description                            | hebrew                         | transliteration
+  ${"no special features"}               | ${"לֶ֬חֶם"}                    | ${"lechem"}
+  ${"gemination"}                        | ${"רַנֵּן"}                    | ${"ranen"}
+  ${"furtive patach, chet"}              | ${"מָשִׁיחַ"}                  | ${"maschiach"}
+  ${"furtive patach, ayin"}              | ${"שָׁמֵ֑עַ"}                  | ${"schamea"}
+  ${"furtive patach, he"}                | ${"גָבֹ֗הַּ"}                  | ${"gawoa"}
+  ${"final chet w/ patach, NOT furtive"} | ${"מִתַּ֣חַת"}                 | ${"mitachat"}
+  ${"final qamets-he"}                   | ${"מַלְכָּה"}                  | ${"malka"}
+  ${"divine name"}                       | ${"יְהוָ֣ה"}                   | ${"Adonaj"}
+  ${"3ms suffix"}                        | ${"תֹּורֹתָֽיו"}               | ${"torotaw"}
+  ${"multiple words and passeq"}         | ${"כְּשֶׁ֣בֶת ׀ הַמֶּ֣לֶךְ"}   | ${"keschewet hamelech"}
+  ${"mixed with latin chars"}            | ${"אֲבֹותֵינוּ (לְעֹולָם)"}    | ${"awotenu (leolam)"}
+  ${"qamets qatan"}                      | ${"כָּל הָעוֹלָם כָּל־הָעֵ֛ץ"} | ${"kol haolam kol-haez"}
 `("$description", ({ description, hebrew, transliteration }) => {
   const heb = new Text(hebrew);
   const transliteratedHeb = heb.transliterate();
